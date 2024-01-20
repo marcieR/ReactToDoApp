1aa6b729750620dbf0351885f017a2708cc296f9
Marcie Price <marcieprice@Marcies-MacBook-Pro.local>
2024-01-20 16:09:45 -0500

    some css changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AddTodo from "../containers/AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
 
 const App = () => (
-  <div>
+  <div className="App">
     <AddTodo />
     <VisibleTodoList />
     <Footer />
--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -18,7 +18,7 @@ const AddTodo = ({ dispatch }) => {
         }}
       >
         <input ref={node => (input = node)} />
-        <button type="submit">Add Todo</button>
+        <button className={'btn'} type="submit">Add Todo</button>
       </form>
     </div>
   );
