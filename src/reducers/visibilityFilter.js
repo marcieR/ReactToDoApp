86ce5f86daf8b0db2a9b8b42a387a7b423df41b1
Marcie Price <marcie.roberts22@gmail.com>
2024-01-20 21:20:19 -0500

    spacing

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,7 +1,7 @@
 import { VisibilityFilters } from "../actions";
 
 const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
-  switch (action.type) {
+  switch ( action.type ) {
     case "SET_VISIBILITY_FILTER":
       return action.filter;
     default:
