703ba3e0648ec9818b4e65690562717bec17b2cb
Marcie Price <marcie.roberts22@gmail.com>
2024-01-21 18:13:39 -0500

    fizzBuzz test problem

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -10,6 +10,22 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
 });
 
+//test fizzBuzz
+function fizzBuzz( n ) {
+  for ( let n = 1; n<=15 ; n++ ) {
+      if( n%15 === 0  ){ 
+          console.log("FizzBuzz");
+      } else if ( n%3 === 0 ) {
+          console.log("Fizz");      
+      } else if ( n%5 === 0 ) {
+          console.log("Buzz");
+      } else {
+          console.log(n);
+      }
+  }
+}
+fizzBuzz();
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
