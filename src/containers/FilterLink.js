d86bc6218220d9d6e56847aef65cb059265d025e
Marcie Price <marcie.roberts22@gmail.com>
2024-01-21 19:19:45 -0500

    adding all items in array

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -14,19 +14,40 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 function fizzBuzz( n ) {
   for ( let n = 1; n<=15 ; n++ ) {
       if( n%15 === 0  ){ 
-          console.log("FizzBuzz");
+          console.log( "FizzBuzz" );
       } else if ( n%3 === 0 ) {
-          console.log("Fizz");      
+          console.log( "Fizz" );      
       } else if ( n%5 === 0 ) {
-          console.log("Buzz");
+          console.log( "Buzz" );
       } else {
-          console.log(n);
+          console.log( n );
       }
   }
 }
 fizzBuzz();
 
+// adding the items in an array
+function sum ( n ) {
+
+  console.log( n );
+  const SOR = n.reduce( add, 0 );
+  console.log ( SOR )
+  function add( accumulator, a ) {
+      return accumulator + a;
+  }
+};
+
+const ar = [
+  1
+  , 2
+  , 3
+  , 4
+  , 10
+  , 11
+]
+sum( ar );
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Link);
+)( Link );
